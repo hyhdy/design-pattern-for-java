3f500aecb6ca6dc370a3b3693b59abea1745b2a2
huangyaohui <huangyaohui@111.com>
2016-11-11 23:07:41 +0800

    observer demo2

---
+++ src/com/observer/demo2/Observer.java
@@ -0,0 +1,6 @@
+package com.observer.demo2;
+
+public interface Observer {
+	public void update();
+
+}
---
+++ src/com/observer/demo2/Subject.java
@@ -0,0 +1,5 @@
+package com.observer.demo2;
+
+public interface Subject {
+	public void notifyUpdate();
+}
